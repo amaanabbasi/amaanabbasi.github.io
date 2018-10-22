cdae2d1870ce22c196ba46289319c1fc4cf3fd17
Neil Chauhan <neilchauhan2@gmail.com>
2018-10-22 22:21:43 +0530

    Updated JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+let animateName = "animated pulse"
+let animationend = "webkitAnimationEnd mozAnimationEnd oAnimationEnd animationEnd"
+let animatedClass = ".title"
+$(animatedClass).mouseenter(function(event){
+    $(this).addClass(animateName);
+});
+$(animatedClass).on(animationend, function(event) {
+    $(this).removeClass(animateName);
+});
